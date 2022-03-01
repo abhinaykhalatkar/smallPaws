96a001fb34d79c742194b8e44eff53887bb8ed16
neelimasahu.cse <neelimasahu.cse@gmail.com>
2022-03-01 20:04:51 +0100

    login page and day care services added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
-import { NavLink } from "react-router-dom";
+import Header from './components/Header';
+import LoginPage from './components/LoginPage';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import DayCareServices from './components/DayCareServices';
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <img src= {'/assets/logo.svg'}/>
-        <NavLink to="/">Adoption</NavLink>
-        <NavLink to="/">Services</NavLink>
-        <NavLink to="/">Help Us</NavLink>
-        <NavLink to="/">About Us</NavLink>
-        <NavLink to="/">Singin / Login</NavLink>
-      </nav>
-      <div className="circle">
-        <img src= {'/assets/circle.svg'}/>
-      </div>
+      <Header/>
+     
+        <Routes>
+          <Route path="/loginPage" element={<LoginPage/>}/>   
+          <Route path="/dayCareServices" element={<DayCareServices/>}/>
+        
+
+        </Routes>    
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavLink } from "react-router-dom";
+
+
+const Header = () => {
+  return (
+    <div>
+    <nav>
+        <img src= {'/assets/logo.svg'}/>
+        <NavLink to="/">Adoption</NavLink>
+        <NavLink to="/dayCareServices">Services</NavLink>
+        <NavLink to="/">Help Us</NavLink>
+        <NavLink to="/">About Us</NavLink>
+        <NavLink to="/loginPage">
+        Sign in / Sign Up
+        </NavLink>
+      </nav>
+      {/* <div className="circle">
+        <img src= {'/assets/circle.svg'}/>
+      </div> */}
+    </div>
+  )
+}
+
+export default Header;
