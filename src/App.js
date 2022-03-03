22e170511cd089568da55f3100b8324250581c85
abhinaykhalatkar <abhinaykhalatkar@gmail.com>
2022-03-03 18:04:54 +0100

    2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import LoginPage from './components/LoginPage';
 import AdoptionCenter from './components/AdoptionCenter/AdoptionCenter';
 import AdoptionDetailsForm from './components/AdoptionDetailsForm/AdoptionDetailsForm';
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import DayCareServices from './components/DayCareServices';
 import Home from "./components/Home/Home";
 
@@ -16,11 +13,11 @@ function App() {
     <div className="App">
       <Header/>
         <Routes>
+        <Route path="/" exact element={<Home/>}/>
           <Route path="/loginPage" element={<LoginPage/>}/>   
           <Route path="/dayCareServices" element={<DayCareServices/>}/>
           <Route path="/adoptionCenter" element={<AdoptionCenter/>}/>
           <Route path="/adoptionDetailsForm" element={<AdoptionDetailsForm/>}/>
-          <Route path="/" exact element={<Home/>}/>
           <Route path="/loginPage" element={<LoginPage/>}/>   
         </Routes>    
     </div>
