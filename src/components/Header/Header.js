165cd289d2585c515112b30d21205d8fabccbb72
virendrak07 <virenkadam07@gmail.com>
2022-03-03 15:33:11 +0530

    dropdown nav added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { NavDropdown } from 'react-bootstrap';
+import { Link, NavLink } from "react-router-dom";
+import './Header.css';
+
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <Link to={`/`}>
+          <img src={'/assets/logo.svg'} alt="logo" />
+        </Link>
+        <div className="navBar">
+          {/* <NavLink to="/">Home</NavLink> */}
+          {/* <NavLink to="/adoptionCenter">Adoption</NavLink> */}
+          <NavDropdown title="Adoption" id="basic-nav-dropdown">
+            <NavDropdown.Item><NavLink to="/adoptionCenter">- Adopt a pet</NavLink></NavDropdown.Item>
+            <NavDropdown.Item><NavLink to="/adoptionCenter">- Put a pet for adoption</NavLink></NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Services">
+            <NavDropdown.Item><NavLink to="/dayCareServices">- Day care</NavLink></NavDropdown.Item>
+            <NavDropdown.Item><NavLink to="/dayCareServices">- Adoption process</NavLink></NavDropdown.Item>
+            <NavDropdown.Item><NavLink to="/dayCareServices">- Vet service</NavLink></NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Help Us">
+            <NavDropdown.Item><NavLink to="/">- Donate</NavLink></NavDropdown.Item>
+            <NavDropdown.Item><NavLink to="/">- Volunteer</NavLink></NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="About Us">
+            <NavDropdown.Item><NavLink to="/">- Achivements</NavLink></NavDropdown.Item>
+            <NavDropdown.Item><NavLink to="/">- Contact us</NavLink></NavDropdown.Item>
+            <NavDropdown.Item><NavLink to="/">- Our expertise</NavLink></NavDropdown.Item>
+          </NavDropdown>
+        </div>
+        <NavLink to="/loginPage">
+          Sign in / Sign Up
+        </NavLink>
+      </nav>
+    </div>
+  )
+}
+
+export default Header;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,7 @@ import { Carousel, Card, CardGroup } from 'react-bootstrap';
 import image1 from '../../Image/image1.jpeg';
 import image2 from '../../Image/image2.jpeg';
 import image3 from '../../Image/image3.jpeg';
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -37,24 +38,30 @@ const Home = () => {
 
       {/* Card board */}
       <CardGroup className="card-group">
-        <Card className="card">
-          <Card.Img variant="top" src={'/assets/card1.svg'} className="card-img-top" />
-          <Card.Body>
-            <Card.Title>Vet Service</Card.Title>
-          </Card.Body>
-        </Card>
-        <Card className="card">
-          <Card.Img variant="top" src={'/assets/card2.svg'} className="card-img-top" />
-          <Card.Body>
-            <Card.Title>Day Care Service</Card.Title>
-          </Card.Body>
-        </Card>
-        <Card className="card">
-          <Card.Img variant="top" src={'/assets/card3.svg'} className="card-img-top" />
-          <Card.Body>
-            <Card.Title>Adoption Center</Card.Title>
-          </Card.Body>
-        </Card>
+        <Link to={`/adoptionCenter`}>
+          <Card className="card">
+            <Card.Img variant="top" src={'/assets/card1.svg'} className="card-img-top" />
+            <Card.Body>
+              <Card.Title>Vet Service</Card.Title>
+            </Card.Body>
+          </Card>
+        </Link>
+        <Link to={`/dayCareServices`}>
+          <Card className="card">
+            <Card.Img variant="top" src={'/assets/card2.svg'} className="card-img-top" />
+            <Card.Body>
+              <Card.Title>Day Care Service</Card.Title>
+            </Card.Body>
+          </Card>
+        </Link>
+        <Link to={`/adoptionCenter`}>
+          <Card className="card">
+            <Card.Img variant="top" src={'/assets/card3.svg'} className="card-img-top" />
+            <Card.Body>
+              <Card.Title>Adoption Center</Card.Title>
+            </Card.Body>
+          </Card>
+        </Link>
       </CardGroup>
     </div>
   );
