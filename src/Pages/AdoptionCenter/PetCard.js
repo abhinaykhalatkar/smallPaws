bbde04df9efd8cf30a8151d7c087b7ce3ff01468
abhinaykhalatkar <abhinaykhalatkar@gmail.com>
2022-03-12 02:00:08 +0100

    added pet cards and prepping them for firebase storage

--- src/Pages/AdoptionCenter/PetCard.js
+++ src/Pages/AdoptionCenter/PetCard.js
@@ -1,22 +1,31 @@
 import "./PetCard.css";
 export default function PetCard(props){
+    let [index,id,url,title]=[...props.petCardDetails]
     return(
-
-        <div className="container">
-        <div className="gallery">
-            {
-                props.petTemplateData.map((el)=>(
-            <div className="gallery-item" key={el.id}>
-                <img className="gallery-image" src={el.url} alt={el.title}/>
-                <p>{el.id%2===0?"DOG":"CAT"}</p>
+        <div>
+        <div className="card__AdCen border-0 me-lg-4 mb-lg-0 mb-4">
+          <div className="backgroundEffect__AdCen"></div>
+          <div className="pic__AdCen">
+            {" "}
+            <img className="" src={url} alt="" />
+            <div className="date">
+              {" "}
+              <span className="day">26</span>{" "}
+              <span className="month">June</span>{" "}
+              <span className="year">2019</span>{" "}
             </div>
-                ))
-            }
+          </div>
+          <div className="content__AdCen">
+            <h6>{id % 2 === 0 ? "DOG" : "CAT"}</h6>
+            <p>{title}</p>
+              <div className="btn__AdCen btn-primary">Details</div>
+          </div>
         </div>
-        
-    </div>
+      </div>
+
     )
 
 }
 
     
+
